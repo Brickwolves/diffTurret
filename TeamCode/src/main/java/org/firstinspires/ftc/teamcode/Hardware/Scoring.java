f7843108611dfd6378b9b5e5108b66e5d443ea70
CollinKeegan <46503780+CollinKeegan@users.noreply.github.com>
2023-02-17 17:16:25 -0500

    We Stay Silly (revert this if everything breaks)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Scoring.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Scoring.java
@@ -198,6 +198,35 @@ public class Scoring {
 
     }
 
+    public class virtual4Bar extends Thread{
+        Scoring scorer;
+        double target;
+        boolean runPID = false;
+        boolean stop = false;
+        public virtual4Bar(Scoring scorer){
+            this.scorer = scorer;
+        }
+        public void setTarget(double target){
+            this.target = target;
+        }
+
+        @Override
+        public void run() {
+            while(!stop && !super.isInterrupted()){
+                if(runPID){
+                    scorer.v4b(target);
+                }
+            }
+        }
+        public void doRun(boolean run){
+            runPID = run;
+        }
+        public void exit(){
+            stop = true;
+            super.interrupt();
+        }
+    }
+
     public void v4bNoSensor(double speed){
         v4b1.setPower(speed);
         v4b2.setPower(-speed);
